cc303b468877f4ce770c7c0038651c56f744ea79
Joyce Ann Pestin <joycepestin@gmail.com>
2020-02-25 18:17:17 +0800

    Third Commit for OOP Prototype

--- js/index.js
+++ js/index.js
@@ -1,13 +1,23 @@
 //Prototype
-let stud1 = {name: "Joyce Ann " , course: "IT" , section: "2ITF" , 
-study:function(subject)
+function Student(name, course, section)
+{
+    this.name = name;
+    this.course = course;
+    this.section = section;
+}
+
+Student.prototype.study = function(subject)
 {
     console.log(`${this.name}, is enrolled in ${subject}`);
-}};
+}
 
-//calling the study method
-//use the objcect syntx
+Student.prototype.school = "UST";
 
-let stud2 = {name: "Pedro" , course: "CS" , section: "2ITE"}; //di gagana kasi wlang study function
+let stud1 = new Student(" Joyce  " , "IT" , "2TF");
+let stud2 = new Student ("Ann " , "CS" , "CSA");
+
+stud1.study("Biology");
+stud2.study("Science");
 
 stud1.study("ICS2609"); 
+console.log(stud1.school);
